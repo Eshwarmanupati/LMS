c9bb4e5d9c2d5143c62b7bebb1dd148735c0a0c6
Eshwarmanupati <eshwarmanupati@gmail.com>
2025-11-02 01:03:40 +0530

    i have updated the CourseDetail page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext()
@@ -17,6 +18,7 @@ export const AppContextProvider = (props) => {
         setAllCourses(dummyCourses)
     }
 
+    // function to calculate average rating of stars
     const calculateRating = (course) => {
         if(course.courseRatings.length === 0){
             return 0;
@@ -27,12 +29,41 @@ export const AppContextProvider = (props) => {
         })
         return totalRating/course.courseRatings.length
     }
+
+    // function to calculate course chapter time
+    const calculateChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+    }
+
+
+    // function to calculate course Duration
+    const calculateCourseDuration = (course) => {
+        let time = onabort;
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+    }
+
+    // function calculate no of lectures in the course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
+    }
+
     useEffect(() => {
         fetchAllCourses()
     },[])
 
     const value = {
-        currency,allCourses,navigate, calculateRating, isEducator,setIsEducator
+        currency,allCourses,navigate, calculateRating, isEducator,setIsEducator,calculateChapterTime,calculateCourseDuration,calculateNoOfLectures
     }
     return(
         <AppContext.Provider value={value}>
