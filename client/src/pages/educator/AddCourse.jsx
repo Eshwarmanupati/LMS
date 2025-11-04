a21e343ec300af9306c62e6320626e09f0530113
Eshwarmanupati <eshwarmanupati@gmail.com>
2025-11-05 01:18:18 +0530

    updated educator pages ui/ux design

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import uniqid from 'uniqid';
+import Quill from 'quill';
 
 const AddCourse = () => {
+
+  const quillRef = useRef(null);
+  const editorRef = useRef(null)
+
+  const [courseTitle, setCourseTitle] = useState('')
+  const [coursePrice, setCoursePrice] = useState(0)
+  const [discount, setDiscount] = useState(0)
+  const [image, setImage] = useState(null)
+  const [chapters, setChapters] = useState([])
+  const [showPopup, setShowPopup] = useState(false)
+  const [currentChapterId, setCurrentChapterId] = useState(null)
+
+  const [lectureDetails, setLectureDetails] = useState({
+    lectureDetails:'',
+    lectureDuration:'',
+    lectureUrl:'',
+    isPreviewFree:false,
+  })
+
+  useEffect(() => {
+    // initial quill only once 
+    if(!quillRef.current && editorRef.current){
+      quillRef.current = new Quill(editorRef.current, {
+        theme:'snow'
+      })
+    }
+  },[])
+
+
   return (
     <div>
       <h1>
